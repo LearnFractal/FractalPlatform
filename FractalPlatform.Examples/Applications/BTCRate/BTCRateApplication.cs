a1a0c7ad24a99df2a3c8ba906b28894b12324f66
Learn Fractal <learn.fractal@gmail.com>
2023-01-21 08:42:42 +0200

    add new app

---
+++ FractalPlatform.Examples/Applications/BTCRate/BTCRateApplication.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using BigDoc.Client.App;
+using BigDoc.Client.UI;
+using BigDoc.Common.Enums;
+using BigDoc.Database.Engine;
+
+namespace FractalPlatform.Examples.Applications.BTCRate
+{
+    public class BTCRateApplication : BaseApplication
+    {
+        public BTCRateApplication(Guid sessionId,
+                                  BigDocInstance instance,
+                                  IFormFactory formFactory) : base(sessionId,
+                                                                   instance,
+                                                                   formFactory)
+        {
+        }
+
+        public override void OnStart(Context context)
+        {
+            var query = REST.Get("https://api.coindesk.com/v1/bpi/currentprice.json")
+                            .ToCollection(context)
+                            .GetFirstDoc(context);
+
+            new
+            {
+                USD = query.Value("{'bpi':{'USD':{'rate':$}}}"),
+                EUR = query.Value("{'bpi':{'EUR':{'rate':$}}}")
+            }
+            .ToCollection(context, Constants.FIRST_DOC_ID)
+            .SetDimension(context, DimensionType.UI, "{'ReadOnly':true,'Style':'Cancel:Refresh'}")
+            .OpenForm(context);
+        }
+    }
+}
---
+++ FractalPlatform.Examples/Applications/BTCRate/README.md
@@ -0,0 +1,18 @@
+# BTC Rate
+
+## Sources
+
+- [Application](https://github.com/LearnFractal/FractalPlatform/tree/main/FractalPlatform.Examples/Applications/BTCRate/BTCRateApplication.cs)
+
+## Functionality
+
+Application calls coindesk.com free REST api to get actual BTC rate and show web form with USD and EUR actual currencies
+
+## How it implemented
+
+**Video explanation**: No video
+
+## Web Link
+
+[BTCRate](https://booben.com/jupiter/?appName=BTCRate)
+
