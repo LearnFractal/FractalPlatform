da4889951e05682ad14cfd0c01790031cfa33a48
Learn Fractal <learn.fractal@gmail.com>
2023-01-27 16:39:23 +0200

    fix

---
+++ FractalPlatform.Examples/Applications/Electricity/ElectricityApplication.cs
@@ -0,0 +1,16 @@
+﻿using BigDoc.Client.App;
+using BigDoc.Client.UI;
+
+namespace FractalPlatform.Examples.Applications.Electricity
+{
+    public class ElectricityApplication : BaseApplication
+    {
+        public override void OnStart()
+        {
+            Client.SetDefaultCollection("Hosts")
+                  .GetFirstDoc()
+                  .WantModifyExistingDocuments()
+                  .OpenForm();
+        }
+    }
+}
