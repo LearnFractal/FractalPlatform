9ebf787d132f7b732a405054c3b7bb1f1c915ee5
Learn Fractal <learn.fractal@gmail.com>
2023-01-31 12:15:42 +0200

    add new app

---
+++ FractalPlatform.Examples/Applications/LoginOrRegister/LoginOrRegisterApplication.cs
@@ -0,0 +1,18 @@
+﻿using BigDoc.Client.App;
+using BigDoc.Database.Engine;
+
+namespace FractalPlatform.Examples.Applications.Login
+{
+    public class LoginOrRegisterApplication : DashboardApplication
+    {
+        protected override void OnRegister(FormResult result)
+        {
+            MessageBox($"You registered as: {User.Name}");
+        }
+
+        protected override void OnLogin(FormResult result)
+        {
+            MessageBox($"You logged in as: {User.Name} with role {User.Roles[0]}");
+        }
+    }
+}
---
+++ FractalPlatform.Examples/Applications/LoginOrRegister/README.md
@@ -0,0 +1,19 @@
+# Login
+
+## Sources
+
+- [Application](https://github.com/LearnFractal/FractalPlatform/tree/main/FractalPlatform.Examples/Applications/Login/LoginApplication.cs)
+- [Database](https://github.com/LearnFractal/FractalPlatform/tree/main/FractalPlatform.Examples/Databases/Login)
+
+## Functionality
+
+Application demonstrates how to works with user credentials
+
+## How it implemented
+
+**Video explanation**: No video
+
+## Web Link
+
+[Login](http://booben.com/jupiter/?appName=Login)
+
