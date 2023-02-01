6083bc9b709092870299807e12a806eb535c7eeb
Learn Fractal <learn.fractal@gmail.com>
2023-02-01 11:45:21 +0200

    fix

---
+++ FractalPlatform.Examples/Applications/RenderPage/RenderPageApplication.cs
@@ -0,0 +1,14 @@
+﻿using BigDoc.Client.App;
+using BigDoc.Client.UI;
+
+namespace FractalPlatform.Examples.Applications.HelloWorld
+{
+    public class RenderPageApplication : BaseApplication
+    {
+        public override void OnStart()
+        {
+            Client.SetDefaultCollection("Dashboard")
+                  .OpenForm();
+        }
+    }
+}
