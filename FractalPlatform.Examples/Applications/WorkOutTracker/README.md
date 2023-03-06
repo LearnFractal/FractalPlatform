4a3296094f351cdf6e77a7183147fbd5a47dc792
Learn Fractal <learn.fractal@gmail.com>
2023-03-06 15:57:26 +0200

    fix changes

---
+++ FractalPlatform.Examples/Applications/WorkOutTracker/README.md
@@ -0,0 +1,22 @@
+# WorkOut tracker for sport results
+
+## Sources
+
+- [Application](https://github.com/LearnFractal/FractalPlatform/tree/main/FractalPlatform.Examples/Applications/WorkOutTracker/WorkOutTrackerApplication.cs)
+- [Database](https://github.com/LearnFractal/FractalPlatform/tree/main/FractalPlatform.Examples/Databases/WorkOutTracker)
+
+## Functionality
+
+Application makes simple sport result tracker with progression results report:
+- Add results up to 4 sets: Set1, Set2, Set3, Set4
+- Calculate results for each training
+- Build report of progression results
+
+## How it implemented
+
+**Video explanation**: No video
+
+## Web Link
+
+[ToDo](http://booben.com/jupiter/?app=WorkOutTracker)
+
---
+++ FractalPlatform.Examples/Applications/WorkOutTracker/WorkOutTrackerApplication.cs
@@ -0,0 +1,67 @@
+﻿using BigDoc.Client.App;
+using BigDoc.Client.App.Chart;
+using BigDoc.Client.UI;
+using BigDoc.Client.UI.DOM;
+using BigDoc.Common.Enums;
+using BigDoc.Database.Engine;
+using BigDoc.Database.Engine.Info;
+using System.Linq;
+
+namespace FractalPlatform.Examples.Applications.ToDo
+{
+    public class WorkOutTrackerApplication : BaseApplication
+    {
+        public override bool OnEventDimension(EventInfo eventInfo)
+        {
+            var exercises = eventInfo.Collection
+                                     .GetAll()
+                                     .Values("{'Results':[{'Exercise':$}]}")
+                                     .Distinct();
+
+            //create chart
+            var chart = new LineGraphsChartInfo
+            {
+                Title = new TitleChartInfo
+                {
+                    Name = "Exersizes",
+                    X = "Time",
+                    Y = "Value"
+                },
+                Lines = exercises.Select(e =>
+                {
+                    uint x = 0;
+
+                    var line = new LineChartInfo
+                    {
+                        Name = e,
+                        Points = eventInfo.Collection
+                                          .GetWhere("{'Results':[{'Exercise':@Exercise}]}", e)
+                                          .IntValues("{'Results':[{'Sum':$}]}")
+                                          .Select(y => new PointChartInfo { X = x++, Y = y })
+                                          .ToList()
+                    };
+
+                    return line;
+                }).ToList()
+            };
+
+            //open report
+            DQL("{'Chart':@Chart}", chart)
+                .ToCollection()
+                .SetDimension(DimensionType.UI, "{'ReadOnly':true,'Chart':{'ControlType':'Chart'}}")
+                .OpenForm();
+
+            return false;
+        }
+
+        public override void OnStart()
+        {
+            Client.SetDefaultCollection("WorkOut")
+                  .GetFirstDoc()
+                  .WantModifyExistingDocuments()
+                  .OpenForm();
+        }
+
+        public override BaseRenderForm CreateRenderForm(DOMForm form) => new ExtendedRenderForm(this, form);
+    }
+}
