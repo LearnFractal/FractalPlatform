8668ca71eb52255fb431453f8029849711ce4541
Learn Fractal <learn.fractal@gmail.com>
2023-05-24 11:47:25 +0300

    add layout tool

---
+++ FractalPlatform.CreateLayout/MainForm.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace FractalPlatform.CreateLayout
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlNavigate = new System.Windows.Forms.Panel();
+            this.btnForward = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnNavigate = new System.Windows.Forms.Button();
+            this.tbNavigate = new System.Windows.Forms.TextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.webView = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.pnlNavigate.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlNavigate
+            // 
+            this.pnlNavigate.Controls.Add(this.btnForward);
+            this.pnlNavigate.Controls.Add(this.btnBack);
+            this.pnlNavigate.Controls.Add(this.btnNavigate);
+            this.pnlNavigate.Controls.Add(this.tbNavigate);
+            this.pnlNavigate.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlNavigate.Location = new System.Drawing.Point(0, 0);
+            this.pnlNavigate.Name = "pnlNavigate";
+            this.pnlNavigate.Size = new System.Drawing.Size(904, 45);
+            this.pnlNavigate.TabIndex = 0;
+            // 
+            // btnForward
+            // 
+            this.btnForward.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnForward.Location = new System.Drawing.Point(814, 3);
+            this.btnForward.Name = "btnForward";
+            this.btnForward.Size = new System.Drawing.Size(87, 35);
+            this.btnForward.TabIndex = 3;
+            this.btnForward.Text = ">>>";
+            this.btnForward.UseVisualStyleBackColor = true;
+            this.btnForward.Click += new System.EventHandler(this.btnGoForward_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBack.Location = new System.Drawing.Point(721, 4);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(87, 33);
+            this.btnBack.TabIndex = 2;
+            this.btnBack.Text = "<<<";
+            this.btnBack.UseVisualStyleBackColor = true;
+            // 
+            // btnNavigate
+            // 
+            this.btnNavigate.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNavigate.Location = new System.Drawing.Point(551, 3);
+            this.btnNavigate.Name = "btnNavigate";
+            this.btnNavigate.Size = new System.Drawing.Size(113, 34);
+            this.btnNavigate.TabIndex = 1;
+            this.btnNavigate.Text = "Navigate";
+            this.btnNavigate.UseVisualStyleBackColor = true;
+            // 
+            // tbNavigate
+            // 
+            this.tbNavigate.Dock = System.Windows.Forms.DockStyle.Left;
+            this.tbNavigate.Location = new System.Drawing.Point(0, 0);
+            this.tbNavigate.Name = "tbNavigate";
+            this.tbNavigate.Size = new System.Drawing.Size(545, 38);
+            this.tbNavigate.TabIndex = 0;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 45);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.webView);
+            this.splitContainer1.Size = new System.Drawing.Size(904, 405);
+            this.splitContainer1.SplitterDistance = 675;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // webView
+            // 
+            this.webView.AllowExternalDrop = true;
+            this.webView.CreationProperties = null;
+            this.webView.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webView.Location = new System.Drawing.Point(0, 0);
+            this.webView.Name = "webView";
+            this.webView.Size = new System.Drawing.Size(675, 405);
+            this.webView.TabIndex = 0;
+            this.webView.ZoomFactor = 1D;
+            this.webView.CoreWebView2InitializationCompleted += new System.EventHandler<Microsoft.Web.WebView2.Core.CoreWebView2InitializationCompletedEventArgs>(this.webView_CoreWebView2InitializationCompleted);
+            this.webView.NavigationStarting += new System.EventHandler<Microsoft.Web.WebView2.Core.CoreWebView2NavigationStartingEventArgs>(this.webView_NavigationStarting);
+            this.webView.WebMessageReceived += new System.EventHandler<Microsoft.Web.WebView2.Core.CoreWebView2WebMessageReceivedEventArgs>(this.webView_WebMessageReceived);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(904, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.pnlNavigate);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.pnlNavigate.ResumeLayout(false);
+            this.pnlNavigate.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlNavigate;
+        private System.Windows.Forms.Button btnForward;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnNavigate;
+        private System.Windows.Forms.TextBox tbNavigate;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView;
+    }
+}
+
---
+++ FractalPlatform.CreateLayout/MainForm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Web.Helpers;
+using System.Windows.Forms;
+
+namespace FractalPlatform.CreateLayout
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            webView.Source = new Uri("https://finance.ua");
+        }
+
+        private async void webView_WebMessageReceived(object sender, Microsoft.Web.WebView2.Core.CoreWebView2WebMessageReceivedEventArgs e)
+        {
+            var html = await webView.CoreWebView2.ExecuteScriptAsync("document.body.outerHTML");
+            var htmldecoded = Json.Decode(html);
+        }
+
+        private void webView_CoreWebView2InitializationCompleted(object sender, Microsoft.Web.WebView2.Core.CoreWebView2InitializationCompletedEventArgs e)
+        {
+            var script = @"document.addEventListener('contextmenu', function (event) {
+                        let elem = document.elementFromPoint(event.clientX, event.clientY);
+                        //alert(document.documentElement.outerHTML);
+                        elem.style.backgroundColor = 'red';
+                        let jsonObject =
+                        {
+                            innerHtml: elem.innerHTML,
+                            ClientX: event.clientX,
+                            ClientY: event.clientY
+                        };
+                        window.chrome.webview.postMessage(jsonObject);
+                        event.preventDefault();
+                    });";
+
+            webView.CoreWebView2.AddScriptToExecuteOnDocumentCreatedAsync(script);
+        }
+
+        private void btnGoBack_Click(object sender, EventArgs e)
+        {
+            webView.GoBack();
+        }
+
+        private void btnGoForward_Click(object sender, EventArgs e)
+        {
+            webView.GoForward();
+        }
+
+        private void webView_NavigationStarting(object sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationStartingEventArgs e)
+        {
+            tbNavigate.Text = e.Uri.ToString();
+        }
+    }
+}
