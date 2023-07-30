785d0e954c58ba40c3a1cbbb1ac06b7363b693f3
Learn Fractal <learn.fractal@gmail.com>
2023-07-30 15:48:39 +0300

    fix

--- FractalPlatform.Examples/Applications/VideoLibrary/VideoLibraryApplication.cs
+++ FractalPlatform.Examples/Applications/VideoLibrary/VideoLibraryApplication.cs
@@ -1,6 +1,7 @@
-﻿using System.IO;
-using System.Linq;
+﻿using System.Linq;
 using FractalPlatform.Client.App;
+using FractalPlatform.Client.UI;
+using FractalPlatform.Common;
 using FractalPlatform.Database.Engine;
 
 namespace FractalPlatform.Examples.Applications.VideoLibrary
@@ -9,21 +10,15 @@ namespace FractalPlatform.Examples.Applications.VideoLibrary
     {
         public override void OnStart()
         {
-            //This code just as example.
-            //The assembly cannot be deployed with security issue (access to hard drive)
-            //to the server without special permissions
-
-            //(means reference to System.IO.Directory.GetFiles)
-            /*
             var obj = new
             {
                 Title = "Video Library",
                 Videos = Directory.GetFiles(@"c:\VideoLibrary", "*.mp4")
                                   .Select(x => new
                                   {
-                                      Title = Path.GetFileName(x),
-                                      Size = $"{new FileInfo(x).Length / 1024 / 1024} mb",
-                                      Video = Path.GetFileName(x)
+                                      Title = Directory.GetFileName(x),
+                                      Size = $"{Directory.GetFileInfo(x).Length / 1024 / 1024} mb",
+                                      Video = Directory.GetFileName(x)
                                   })
             };
 
@@ -31,7 +26,7 @@ namespace FractalPlatform.Examples.Applications.VideoLibrary
                   .GetFirstDoc()
                   .ExtendDocument(obj.ToJson())
                   .OpenForm();
-            */
+            
         }
     }
 }
