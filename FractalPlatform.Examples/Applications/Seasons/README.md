d39aeef8a2a90fb434d92696e6cc82180d316ab2
Bazist <learn.fractal@gmail.com>
2023-09-04 09:25:38 +0300

    fix

---
+++ FractalPlatform.Examples/Applications/Seasons/README.md
@@ -0,0 +1,25 @@
+# Seosons
+
+## Sources
+
+- [Application](https://github.com/LearnFractal/FractalPlatform/tree/main/FractalPlatform.Examples/Applications/Seasons/SeasonsApplication.cs)
+- [Files](https://github.com/LearnFractal/FractalPlatform/tree/main/FractalPlatform.Examples/Files/Seasons)
+- [Database](https://github.com/LearnFractal/FractalPlatform/tree/main/FractalPlatform.Examples/Databases/Seasons)
+
+## Functionality
+
+Application demonstrates online theatre with series and episodes with next functionality:
+
+- Read list of seasons from the server
+- Create web site that contains list of videos with pagination
+- User is available to search episodes by name in the search text box
+- User can view a episodes in the web browser
+
+## How it implemented
+
+**Video explanation**: No video
+
+## Web Link
+
+[Seasons](https://fraplat.com/jupiter/Seasons)
+
---
+++ FractalPlatform.Examples/Applications/Seasons/SeasonsApplication.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using FractalPlatform.Client.App;
+using FractalPlatform.Client.UI;
+using FractalPlatform.Common;
+using FractalPlatform.Database.Engine;
+
+namespace FractalPlatform.Examples.Applications.VideoLibrary
+{
+    public class SeasonsApplication : BaseApplication
+    {
+        public override void OnStart()
+        {
+            var obj = new
+            {
+                Title = "Seasons",
+                Seasons = Directory.GetDirectories(@"d:\Seasons")
+                                   .Select(x => new 
+                                   {
+                                      Series = x,
+                                      Episodes = Directory.GetFiles(x, "*.mp4")
+                                                          .Select(x => new
+                                                          {
+                                                            Title = Directory.GetFileName(x),
+                                                            Size = $"{Directory.GetFileInfo(x).Length / 1024 / 1024} mb",
+                                                            Video = Directory.GetFileName(x)
+                                                          })
+                                   })
+                
+                
+            };
+
+            Client.SetDefaultCollection("Series")
+                  .GetFirstDoc()
+                  .ExtendDocument(obj.ToJson())
+                  .OpenForm();
+            
+        }
+    }
+}
