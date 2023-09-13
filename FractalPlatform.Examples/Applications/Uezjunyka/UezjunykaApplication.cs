e73cdc404bd53ad73afeb02ee0eaa2cb1e0c6607
Learn Fractal <learn.fractal@gmail.com>
2023-09-13 15:01:52 +0300

    upload new landing example

---
+++ FractalPlatform.Examples/Applications/Uezjunyka/UezjunykaApplication.cs
@@ -0,0 +1,24 @@
+﻿using FractalPlatform.Client.App;
+using FractalPlatform.Client.UI;
+
+namespace FractalPlatform.Examples.Applications.Uezjunyka
+{
+    public class UezjunykaApplication : BaseApplication
+    {
+        public override void OnStart()
+        {
+            Client.SetDefaultCollection("NewUezjunyka")
+                  .WantCreateNewDocumentFor("Uezjunyka")
+                  .OpenForm(result => {
+                      MessageBox("Спасибо, Вы зарегестрированы !", "Регистрация",
+                                 MessageBoxButtonType.Ok,
+                                 result =>
+                      {
+                          Client.SetDefaultCollection("Uezjunyka")
+                                .GetAll()
+                                .OpenForm();
+                      });
+                  });
+        }
+    }
+}
