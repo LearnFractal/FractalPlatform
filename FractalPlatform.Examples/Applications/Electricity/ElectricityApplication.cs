a1f23a1f8c3f84a7d2ab10947d3d6c974dd99ec5
Learn Fractal <learn.fractal@gmail.com>
2023-09-27 15:44:27 +0300

    Electricity project

--- FractalPlatform.Examples/Applications/Electricity/ElectricityApplication.cs
+++ FractalPlatform.Examples/Applications/Electricity/ElectricityApplication.cs
@@ -1,5 +1,6 @@
 ﻿using FractalPlatform.Client.App;
 using FractalPlatform.Client.UI;
+using FractalPlatform.Client.UI.DOM;
 using FractalPlatform.Database.Engine.Info;
 using System.Net.NetworkInformation;
 
@@ -24,18 +25,20 @@ namespace FractalPlatform.Examples.Applications.Electricity
 
             foreach (var location in locations)
             {
-                if (location.HasElectricity != PingHost(location.IPAddress))
+                if (location.HasElectricity != PingHost(location.IPAddress) && location.TelegramUserID > 0)
                 {
                     location.HasElectricity = !location.HasElectricity;
 
                     Client.SetDefaultCollection("Locations")
                           .GetWhere("{'Address':@Address}", location.Address)
-                          .Update(@"{'TextMessages':[Add,{'Provider':'Telegram',
-                                                      'Receiver':@Receiver,
-                                                      'Message':@Message,
-                                                      'IsSent':false}]}",
+                          .Update(@"{'HasElectricity':@HasElectricity,
+                                     'TextMessages':[Add,{'Provider':'Telegram',
+                                                          'Receiver':@Receiver,
+                                                          'Message':@Message,
+                                                          'IsSent':false}]}",
+                                    location.HasElectricity,
                                     location.TelegramUserID,
-                                    location.HasElectricity ? "Electricity available" : "No electricity");
+                                    location.HasElectricity ? $"{location.Address} HAS electricity" : $"{location.Address} HAS NO electricity");
                 }
 
                 Client.SetDefaultCollection("Locations")
@@ -64,6 +67,8 @@ namespace FractalPlatform.Examples.Applications.Electricity
                                   Client.SetDefaultCollection("Dashboard")
                                         .GetFirstDoc()
                                         .Update("{'Map':{'Points':[Add,{'Lat':@Lat,'Lng':@Lng}]}}", gps[0], gps[1]);
+
+                                  result.NeedReloadData = true;
                               }
                           });
                     return true;
@@ -83,5 +88,7 @@ namespace FractalPlatform.Examples.Applications.Electricity
                   .GetFirstDoc()
                   .OpenForm();
         }
+
+        public override BaseRenderForm CreateRenderForm(DOMForm form) => new ExtendedRenderForm(this, form);
     }
 }
