e4d25ff51a01b28f86eb735814a2923059fab238
Bazist <learn.fractal@gmail.com>
2023-09-27 09:57:40 +0300

    add Electricity app

---
+++ FractalPlatform.Examples/Applications/Electricity/ElectricityApplication.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FractalPlatform.Client.App;
+using FractalPlatform.Client.UI;
+using FractalPlatform.Client.UI.DOM;
+using FractalPlatform.Common.Enums;
+using FractalPlatform.Database.Engine;
+using FractalPlatform.Database.Engine.Info;
+
+namespace FractalPlatform.Examples.Applications.Electricity
+{
+    public class ElectricityApplication : BaseApplication
+    {
+        public override void OnStart()
+        {
+        
+        }
+    }
+}
