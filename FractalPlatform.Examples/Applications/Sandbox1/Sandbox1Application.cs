c6fc2a5a827cf5dc038fa32eb3397696d586a285
Bazist <learn.fractal@gmail.com>
2023-10-27 23:45:55 +0300

    fix

--- FractalPlatform.Examples/Applications/Sandbox1/Sandbox1Application.cs
+++ FractalPlatform.Examples/Applications/Sandbox1/Sandbox1Application.cs
@@ -3,31 +3,30 @@ using FractalPlatform.Client.UI;
 using FractalPlatform.Common;
 using FractalPlatform.Database.Engine;
 using FractalPlatform.Database.Engine.Info;
+using System.Linq;
 
 namespace FractalPlatform.Examples.Applications.Sandbox1
 {
     public class Sandbox1Application : BaseApplication
     {
-        public override object OnComputedDimension(ComputedInfo computedInfo)
+        private class Product 
         {
-            bool isValid = true;
-
-            Validators.ValidateJson(computedInfo.AttrValue.ToString(), (x, y, z) => isValid = false);
-
-            return isValid;
+            public string Name { get; set; }
+            public int Quantity { get; set; }
+            public decimal Price { get; set; }
         }
 
         public override void OnStart()
         {
-            Client.SetDefaultCollection("Dashboard")
-                  .GetFirstDoc()
-                  .OpenForm(result =>
-                  {
-                      var json = result.Collection.FindFirstValue("JSON");
-
-                      json.ToCollection(Constants.FIRST_DOC_ID)
-                          .OpenForm(result => OnStart());
-                  });
+            new { Products = new[] 
+                    { new Product { Name = "Хліб", Quantity = 1, Price = 1 }, 
+                      new Product { Name = "Молоко", Quantity = 2, Price = 5 } } }
+            .ToCollection(Constants.FIRST_DOC_ID)
+            .SetUIDimension("{'Style':'Save:Calculate;Cancel:false'}")
+            .OpenForm(result => {
+                var price = result.Collection.GetFirstDoc().Select<Product>("{'Products':[!$]}").Sum(x => x.Quantity * x.Price);
+                MessageBox($"Рахунок: {price} грн.");
+            });
         }
     }
 }
