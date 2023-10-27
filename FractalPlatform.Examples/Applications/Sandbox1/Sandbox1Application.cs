60a7501e279680d1ec5a854b4d3aa3d4d9df8721
Bazist <learn.fractal@gmail.com>
2023-10-28 00:02:55 +0300

    fix sandbox

--- FractalPlatform.Examples/Applications/Sandbox1/Sandbox1Application.cs
+++ FractalPlatform.Examples/Applications/Sandbox1/Sandbox1Application.cs
@@ -18,14 +18,12 @@ namespace FractalPlatform.Examples.Applications.Sandbox1
 
         public override void OnStart()
         {
-            new { Products = new[] 
-                    { new Product { Name = "Хліб", Quantity = 1, Price = 1 }, 
-                      new Product { Name = "Молоко", Quantity = 2, Price = 5 } } }
-            .ToCollection(Constants.FIRST_DOC_ID)
-            .SetUIDimension("{'Style':'Save:Calculate;Cancel:false'}")
-            .OpenForm(result => {
-                var price = result.Collection.GetFirstDoc().Select<Product>("{'Products':[!$]}").Sum(x => x.Quantity * x.Price);
-                MessageBox($"Рахунок: {price} грн.");
+            Client.SetDefaultCollection("Products")
+                  .GetFirstDoc()
+                  .WantModifyExistingDocuments()
+                  .OpenForm(result => {
+                        var price = result.Collection.GetFirstDoc().Select<Product>("{'Products':[!$]}").Sum(x => x.Quantity * x.Price);
+                        MessageBox($"Рахунок: {price} грн.");
             });
         }
     }
