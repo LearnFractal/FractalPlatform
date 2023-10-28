627fb3af8eea8c0583e032d0ca322fa12964fea8
Bazist <learn.fractal@gmail.com>
2023-10-28 18:33:20 +0300

    add proj

---
+++ FractalPlatform.Seasons/SeasonsApplication.cs
@@ -0,0 +1,25 @@
+﻿using FractalPlatform.Client.App;
+
+namespace FractalPlatform.Seasons
+{
+    public class SeasonsApplication : BaseApplication
+    {
+        private class Product
+        {
+            public string Name { get; set; }
+            public int Quantity { get; set; }
+            public decimal Price { get; set; }
+        }
+
+        public override void OnStart()
+        {
+            //ModifyFirstDocOf("Products").OpenForm(result => {
+            //    var price = result.Collection.GetFirstDoc().Select<Product>("{'Products':[!$]}").Sum(x => x.Quantity * x.Price);
+            //    MessageBox($"Рахунок: {price} грн.");
+            //});
+
+            MessageBox("Hello World !");
+        }
+    }
+}
+
