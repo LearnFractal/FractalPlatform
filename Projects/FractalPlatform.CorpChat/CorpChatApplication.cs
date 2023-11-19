01808235c16e1b789a7f8fe20a0a141d88323656
Bazist <learn.fractal@gmail.com>
2023-11-19 22:03:16 +0200

    add new CorpChat project

---
+++ Projects/FractalPlatform.CorpChat/CorpChatApplication.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using FractalPlatform.Client.App;
+using FractalPlatform.Client.UI;
+using FractalPlatform.Client.UI.DOM;
+using FractalPlatform.Database.Engine;
+using FractalPlatform.Database.Engine.Info;
+using FractalPlatform.Database.Engine.Query;
+
+namespace FractalPlatform.CorpChat
+{
+    public class CorpChatApplication : BaseApplication
+    {
+        public override void OnStart()
+        {
+        
+        }
+
+        public override BaseRenderForm CreateRenderForm(DOMForm form) => new ExtendedRenderForm(this, form);
+    }
+}
---
+++ Projects/FractalPlatform.CorpChat/README.md
@@ -0,0 +1,19 @@
+# Weather
+
+## Sources
+
+- [Application](https://github.com/LearnFractal/FractalPlatform/tree/main/FractalPlatform.Examples/Applications/Weather/WeatherApplication.cs)
+
+## Functionality
+
+Application shows web form based on REST api call.
+In current case application shows forecast weather on the next week by gps coordinates.
+
+## How it implemented
+
+**Video explanation**: No video
+
+## Web Link
+
+[Weather](https://fraplat.com/jupiter/?app=Weather)
+
