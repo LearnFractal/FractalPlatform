39f63d0d8333660bea0a300fe56f9096807f4d66
Bazist <learn.fractal@gmail.com>
2024-02-12 10:03:25 +0200

    add diary app

---
+++ FractalPlatform.Examples/Applications/Diary/DiaryApplication.cs
@@ -0,0 +1,29 @@
+﻿using FractalPlatform.Client.App;
+using FractalPlatform.Client.UI;
+using FractalPlatform.Database.Engine.Info;
+
+namespace FractalPlatform.Examples.Applications.Diary
+{
+    public class DiaryApplication : BaseApplication
+    {
+        public override bool OnEventDimension(EventInfo eventInfo)
+        {
+            switch (eventInfo.Action)
+            {
+                case "NewDay":
+                    CreateNewDocFor("NewDay", "Days").OpenForm();
+                    return true;
+                case "Days":
+                    DocsOf("Days").OpenForm();
+                    return true;
+                default:
+                    return base.OnEventDimension(eventInfo);
+            }
+        }
+
+        public override void OnStart()
+        {
+            FirstDocOf("Dashboard").OpenForm();
+        }
+    }
+}
