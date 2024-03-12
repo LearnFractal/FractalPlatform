2e0d291784fe90668624ffb4e749e6e0f56eed1e
Bazist <learn.fractal@gmail.com>
2024-03-12 21:30:05 +0200

    add TextToQRCode web app

---
+++ FractalPlatform.Examples/Applications/TextToQRCode/TextToQRCodeAppApplication.cs
@@ -0,0 +1,26 @@
+﻿using FractalPlatform.Client.App;
+using FractalPlatform.Client.UI;
+using FractalPlatform.Database.Engine;
+
+namespace FractalPlatform.Examples.Applications.TextToQRCode
+{
+    public class TextToQRCodeAppApplication : BaseApplication
+    {
+        public override void OnStart()
+        {
+            FirstDocOf("Dashboard")
+                  .OpenForm(result =>
+                  {
+                      new
+                      {
+                          QRCode = result.Collection
+                                         .GetFirstDoc()
+                                         .Value("{'Text':$}")
+                      }
+                      .ToCollection(Constants.FIRST_DOC_ID, string.Empty)
+                      .SetUIDimension("{'QRCode':{'ControlType':'Picture','Style':'Type:QRCode'}}")
+                      .OpenForm();
+                  });
+        }
+    }
+}
