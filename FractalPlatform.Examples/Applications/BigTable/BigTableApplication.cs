6b2e3ebaba6cf76d5f3a119262f0f881e560496c
Bazist <learn.fractal@gmail.com>
2024-03-18 20:34:34 +0200

    add new test project

---
+++ FractalPlatform.Examples/Applications/BigTable/BigTableApplication.cs
@@ -0,0 +1,16 @@
+﻿using FractalPlatform.Client.App;
+using FractalPlatform.Client.UI;
+
+namespace FractalPlatform.Examples.Applications.BigTable
+{
+    public class BigTableApplication : BaseApplication
+    {
+        public override void OnStart()
+        {
+            DocsOf("Orders").OpenForm(result => 
+            {
+                CreateNewDocFor("NewOrder", "Orders").OpenForm();
+            });
+        }
+    }
+}
