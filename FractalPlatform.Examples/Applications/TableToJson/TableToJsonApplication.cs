1d960615b7fc06f5f59537463acc68214688a336
Bazist <learn.fractal@gmail.com>
2024-05-25 00:13:24 +0300

    table to json

--- FractalPlatform.Examples/Applications/TableToJson/TableToJsonApplication.cs
+++ FractalPlatform.Examples/Applications/TableToJson/TableToJsonApplication.cs
@@ -1,5 +1,6 @@
 ﻿using FractalPlatform.Client.App;
 using FractalPlatform.Client.UI;
+using System.Text;
 
 namespace FractalPlatform.Examples.Applications.TableToJson
 {
@@ -14,9 +15,38 @@ namespace FractalPlatform.Examples.Applications.TableToJson
                                         .GetFirstDoc()
                                         .Value("{'Table':$}");
 
-                      /*
-                                  
-                      */
+                      var lines = table.Split('\n');
+
+                      var columns = lines[0].Split('\t');
+
+                      var sb = new StringBuilder();
+
+                      sb.Append("{'Documents':[");
+
+                      for (int i = 1; i < lines.Length; i++)
+                      {
+                          sb.Append('{');
+
+                          var cells = lines[i].Split("\t");
+
+                          for (int j = 0; j < columns.Length; j++)
+                          {
+                              if (j > 0)
+                              {
+                                  sb.Append(',');
+                              }
+
+                              sb.Append($"'{columns[j].Trim()}': '{cells[j].Trim()}'");
+                          }
+
+                          sb.Append('}');
+                      }
+
+                      sb.Append("]}");
+
+                      var json = sb.ToString();
+
+                      MessageBox(json);
                   });
         }
     }
