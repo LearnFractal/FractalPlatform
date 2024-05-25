bc95dec611a1cce9d4a6ab5a802a706a4ef2a4a4
Bazist <learn.fractal@gmail.com>
2024-05-25 08:44:39 +0300

    fix some bugs

--- FractalPlatform.Examples/Applications/TableToJson/TableToJsonApplication.cs
+++ FractalPlatform.Examples/Applications/TableToJson/TableToJsonApplication.cs
@@ -1,52 +1,74 @@
 ﻿using FractalPlatform.Client.App;
 using FractalPlatform.Client.UI;
+using FractalPlatform.Database.Engine;
+using FractalPlatform.Common.Serialization;
 using System.Text;
 
 namespace FractalPlatform.Examples.Applications.TableToJson
 {
     public class TableToJsonApplication : BaseApplication
     {
+        private string TableToJson(string text)
+        {
+            var lines = text.Split('\n');
+
+            var columns = lines[0].Split('\t');
+
+            var sb = new StringBuilder();
+
+            sb.Append("{'Rows':[");
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                var cells = lines[i].Split("\t");
+
+                if (columns.Length == cells.Length)
+                {
+                    if (i > 1)
+                    {
+                        sb.Append(',');
+                    }
+
+                    sb.Append('{');
+
+                    for (int j = 0; j < columns.Length; j++)
+                    {
+                        if (j > 0)
+                        {
+                            sb.Append(',');
+                        }
+
+                        sb.Append($"\"{columns[j].Trim()}\": \"{cells[j].Trim()}\"");
+                    }
+
+                    sb.Append('}');
+                }
+            }
+
+            sb.Append("]}");
+
+            return JsonConvert.FormatJson(sb.ToString());
+        }
+
         public override void OnStart()
         {
             FirstDocOf("Dashboard")
                   .OpenForm(result =>
                   {
-                      var table = result.Collection
-                                        .GetFirstDoc()
-                                        .Value("{'Table':$}");
+                      var fileName = result.Collection
+                                           .GetFirstDoc()
+                                           .Value("{'Table':$}");
 
-                      var lines = table.Split('\n');
+                      var text = ReadFileText(fileName);
 
-                      var columns = lines[0].Split('\t');
+                      var json = TableToJson(text);
 
-                      var sb = new StringBuilder();
+                      WriteFileText(fileName, json);
 
-                      sb.Append("{'Documents':[");
-
-                      for (int i = 1; i < lines.Length; i++)
-                      {
-                          sb.Append('{');
-
-                          var cells = lines[i].Split("\t");
-
-                          for (int j = 0; j < columns.Length; j++)
-                          {
-                              if (j > 0)
-                              {
-                                  sb.Append(',');
-                              }
-
-                              sb.Append($"'{columns[j].Trim()}': '{cells[j].Trim()}'");
-                          }
-
-                          sb.Append('}');
-                      }
-
-                      sb.Append("]}");
-
-                      var json = sb.ToString();
-
-                      MessageBox(json);
+                      DQL("{'Download':@FileName}", fileName)
+                        .ToCollection()
+                        .SetUIDimension("{'Style':'Save:false','Download':{'ControlType':'Link'}}")
+                        .OpenForm();
                   });
         }
     }
