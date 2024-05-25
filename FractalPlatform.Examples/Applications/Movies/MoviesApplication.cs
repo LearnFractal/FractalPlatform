a3959254bdb4d72beb5d58866ab5a9cb53bed42d
Bazist <learn.fractal@gmail.com>
2024-05-26 00:01:37 +0300

    fix movies

--- FractalPlatform.Examples/Applications/Movies/MoviesApplication.cs
+++ FractalPlatform.Examples/Applications/Movies/MoviesApplication.cs
@@ -3,11 +3,43 @@ using FractalPlatform.Client.App;
 using FractalPlatform.Client.UI;
 using FractalPlatform.Common;
 using FractalPlatform.Database.Engine;
+using FractalPlatform.Database.Engine.Info;
 
 namespace FractalPlatform.Examples.Applications.Movies
 {
     public class MoviesApplication : BaseApplication
     {
+        private object _obj = new
+        {
+            Title = "Watch all seasons",
+            Seasons = Directory.GetDirectories(@"d:\Movies")
+                                   .Select(d => new
+                                   {
+                                       Series = Directory.GetFileName(d),
+                                       Episodes = Directory.GetFiles(d, "*.mp4")
+                                                           .Select(f => new
+                                                           {
+                                                               Title = Directory.GetFileName(f),
+                                                               Size = $"{Directory.GetFileInfo(f).Length / 1024 / 1024} mb",
+                                                               Episode = @$"{Directory.GetDirectoryInfo(d).Name}\{Directory.GetFileName(f)}",
+                                                               NextEpisode = "Next episode"
+                                                           })
+                                   })
+        };
+
+        public override bool OnEventDimension(EventInfo eventInfo)
+        {
+            var idx = eventInfo.AttrPath.Key.GetLastIndexLevel2();
+
+            eventInfo.AttrPath.Key.SetLastIndexLevel2(idx++);
+
+            FirstDocOf("Series")
+                   .ExtendDocument(_obj.ToJson())
+                   .OpenForm(eventInfo.AttrPath.Key.ToDQL2());
+
+            return true;
+        }
+
         public override void OnStart()
         {
             InputBox("Password", "Enter password", result =>
@@ -18,25 +50,8 @@ namespace FractalPlatform.Examples.Applications.Movies
                               .GetFirstDoc()
                               .IsEquals("{'Password':$}", "ps"))
                     {
-                        var obj = new
-                        {
-                            Title = "Watch all seasons",
-                            Seasons = Directory.GetDirectories(@"d:\Movies")
-                                   .Select(d => new
-                                   {
-                                       Series = Directory.GetFileName(d),
-                                       Episodes = Directory.GetFiles(d, "*.mp4")
-                                                           .Select(f => new
-                                                           {
-                                                              Title = Directory.GetFileName(f),
-                                                              Size = $"{Directory.GetFileInfo(f).Length / 1024 / 1024} mb",
-                                                              Episode = @$"{Directory.GetDirectoryInfo(d).Name}\{Directory.GetFileName(f)}"
-                                                           })
-                                   })
-                        };
-
                         FirstDocOf("Series")
-                              .ExtendDocument(obj.ToJson())
+                              .ExtendDocument(_obj.ToJson())
                               .OpenForm();
                     }
                     else
