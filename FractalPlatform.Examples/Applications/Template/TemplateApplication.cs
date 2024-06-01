3fabff9d2fc080d1463a3a37cb83d269c80c1761
Bazist <learn.fractal@gmail.com>
2024-06-01 09:55:00 +0300

    template

---
+++ FractalPlatform.Examples/Applications/Template/TemplateApplication.cs
@@ -0,0 +1,13 @@
+﻿using FractalPlatform.Client.App;
+using FractalPlatform.Client.UI;
+
+namespace FractalPlatform.Examples.Applications.Template
+{
+    public class TeplateApplication : BaseApplication
+    {
+        public override void OnStart()
+        {
+            ModifyFirstDocOf("TemplateList").OpenForm();
+        }
+    }
+}
