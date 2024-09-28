fa1df72709594955b2e92625519223152cdf877d
Bazist <learn.fractal@gmail.com>
2024-09-28 19:39:05 +0300

    add AIWhatYouSee app

---
+++ FractalPlatform.Examples/Applications/AIWhatYouSee/AIWhatYouSeeAppApplication.cs
@@ -0,0 +1,25 @@
+﻿using FractalPlatform.Client.App;
+using FractalPlatform.Client.UI;
+using FractalPlatform.Database.Clients;
+
+namespace FractalPlatform.Examples.Applications.AIWhatYouSee
+{
+	public class AIWhatYouSeeApplication : BaseApplication
+	{
+		public override void OnStart() =>
+			FirstDocOf("Dashboard").OpenForm(result =>
+			{
+				if (result.Result)
+				{
+					var question = result.Collection.FindFirstValue("Question");
+					var image = result.Collection.FindFirstValue("Image");
+
+					var bytes = ReadFileBytes(image);
+
+					var answer = AI.Generate(question, AIModel.Llava, AIImage.FromBytes(bytes));
+
+					MessageBox(answer.Text, MessageBoxButtonType.Ok);
+				}
+			});
+	}
+}
--- FractalPlatform.Examples/Applications/Sandbox2/Sandbox2Application.cs
+++ FractalPlatform.Examples/Applications/Sandbox2/Sandbox2Application.cs
@@ -2,7 +2,9 @@
 using FractalPlatform.Client.UI;
 using FractalPlatform.Database.Clients;
 using FractalPlatform.Database.Engine;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace FractalPlatform.Examples.Applications.Sandbox2
 {
@@ -10,9 +12,20 @@ namespace FractalPlatform.Examples.Applications.Sandbox2
     {
         public override void OnStart() 
         {
-            var bytes = File.ReadAllBytes(@"C:\Users\tuzvy\OneDrive\Pictures\Denmark.jpg");
+			var bytes = File.ReadAllBytes(@"C:\Users\tuzvy\OneDrive\Pictures\Me4.jpg");
 
-            var text = AI.Generate("What you see on this image ?", AIModel.GPT4o, AIImage.FromBytes(bytes));
+            var list = new List<bool>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                var text = AI.Generate("Do you see a person who lies on the picture. Just say YES or NO",
+                                        AIModel.Llava,
+                                        AIImage.FromBytes(bytes));
+
+                list.Add(text.Text.ToLower().Contains("yes"));
+            }
+
+            var count = list.Count(x => x);
         
         }
     }
